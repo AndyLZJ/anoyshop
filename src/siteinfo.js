a11beb9b484c08bd9f0108b9736acbb1225a1120
qinuoyun <qinuoyun@qq.com>
2021-06-17 11:38:05 +0800

    删除冗余代码

--- src/siteinfo.js
+++ src/siteinfo.js
@@ -9,11 +9,7 @@ module.exports = {
     "uniacid": "4",
     "acid": "3",
     "multiid": "0",
-    "version": "1.2.0",
-    "AppURL": "http://www.qmpaas.com/index.php",
-    // "siteroot": "http://www.qmpaas.com/index.php",
-    "siteroot": "https://dev.91bd.cn/index.php",
-    // "siteroot": "https://test.leadshop.vip/index.php",
-    // "siteroot": "http://localhost:8080/index.php",
+    "version": "1.2.7",
+    "siteroot": "",
     "design_method": "3"
 }
